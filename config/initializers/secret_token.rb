3b611434f5bbfde521dfc85e0460338544885268
Hubert Wong <hubertwwong@gmail.com>
2014-02-19 19:59:34 -0800

    rspec install.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zittter01::Application.config.secret_key_base = 'e8896cf77ecdd2285ee59e00a0fd15e27a9f6136ef64147a9989fabbf29bea7eb5c071c0bda55b761f15c678545ed355344afb4cc03bf1d971fa08b299f84985'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zittter01::Application.config.session_store :cookie_store, key: '_zittter01_session'
