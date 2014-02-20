6853058d82ee0038075ff55459954a38e9496762
Hubert Wong <hubertwwong@gmail.com>
2014-02-20 15:30:42 -0800

    added a readme. working on index action for tweet.

---
+++ README.md
@@ -0,0 +1,5 @@
+zittter
+
+Just a practice project to brush up on rails 4 and the new things going on.
+Probably will be put up on heroku when i'm done with it. Basically a twitter
+clone.
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,25 @@
 class TweetsController < ApplicationController
+
+  before_filter :authenticate_user!
+
+  def create
+    # debugging
+    render text: params[:post].inspect
+  end
+
+  def index
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+  
+  
+  
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:user_id, :content)
+  end
+  
 end
